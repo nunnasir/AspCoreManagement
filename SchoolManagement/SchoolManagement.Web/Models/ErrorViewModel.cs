3e2bb96210729d3e2fd28ff8e36da0955f24302d
nasir <nunnasir@gmail.com>
2020-05-08 18:07:50 +0600

    Common Template With Theme/AdminLTE-3.0.2

---
+++ SchoolManagement/SchoolManagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchoolManagement.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
