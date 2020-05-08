1da3fa595a628b12da1b6a43778383128aa00084
nasir <nunnasir@gmail.com>
2020-05-08 17:58:56 +0600

    Initial Project Setup with custom theme

---
+++ SchoolManagement/SchoolManagement.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.Web.Areas.Admin.Models;
+
+namespace SchoolManagement.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new DashboardModel();
+            return View(model);
+        }
+    }
+}
